6cbd16618b516077821adc9e1315c60be6ebbe6b
nguyendangcode <dinh2671@gmail.com>
2025-05-25 10:11:18 +0700

    Update main.c

--- lesson1/main.c
+++ lesson1/main.c
@@ -1,30 +1,14 @@
 #include <stdio.h>
-#include <math.h>
-void phuong_trinh_bac_2(float a,float b,float c) {
-	float delta = b * b - 4 * a * c;
-	if (a != 0) {
-		if (delta < 0) {
-			printf("Phuong trinh vo nghiem\n");
-		}
-		else if (delta == 0) {
-			float x = -b / (2 * a);
-			printf("Phuong trinh co 1 nghiem: x = %.2f\n", x);
-		}
-		else {
-			float x1 = (-b + sqrt(delta)) / (2 * a);
-			float x2 = (-b - sqrt(delta)) / (2 * a);
-			printf("Phuong trinh co 2 nghiem: x1 = %.2f, x2 = %.2f\n", x1, x2);
-		}
+void in_tong() {
+	int n = 0;
+	printf("Nhap n: ");
+	scanf_s("%d", &n);
+	long long tong = 0;
+	for (int i = 1; i <= n; i++) {
+		tong += i;
 	}
-	else {
-		if (b != 0) {
-			float x1 = -c / b;
-			printf("phuong trinh co 1 nghiem: %0.1f", x1);
-		}
-		else printf("phuong trinh vo so nghiem\n");
-	}
+	printf("Tong %d la: %lld\n", n, tong);
 }
-int main() {
-	phuong_trinh_bac_2(0, 0, -50); 
-	return 0;
+void main(void) {
+	in_tong();
 }
